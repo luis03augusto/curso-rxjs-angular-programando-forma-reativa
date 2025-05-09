91ebe1fe623835a8330d46ca16ddaf5f8e45c43d
LuisCurado <luisaugusto.trindadecurado@outlook.com>
2025-05-08 21:31:12 -0300

    curso RxJS

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { catchError, debounceTime, filter, map, switchMap, tap, throwError } from 'rxjs';
+import { Item, Livro, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolume } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSE = 300;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -7,9 +13,33 @@ import { Component } from '@angular/core';
 })
 export class ListaLivrosComponent {
 
-  listaLivros: [];
+  livrosResultado: LivrosResultado;
+  campoBusca = new FormControl();
+  mensagemErro = '';
 
-  constructor() { }
+  constructor(private service: LivroService) { }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSE),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map((resultado) => resultado.items ?? []),
+      map((item) => this.livrosResultadoParaLivros(item)),
+      catchError((erro) => {
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação!'));
+      })
+    )
+
+  livrosResultadoParaLivros(items: Item[]): Livro[]{
+    return items.map(item => {
+      return new LivroVolume(item)
+    });
+  }
 
 }
 
